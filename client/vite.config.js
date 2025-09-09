ca55552d963812545c88125e9901029cffefa7ae
Shemrei Marabillo <shemrei@whyleavetown.com>
2025-09-09 13:00:54 +0800

    added docker but not yet executed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -10,4 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    port: 4000,
+  },
+});
