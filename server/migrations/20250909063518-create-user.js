da053516b411e3e58504b4c2a047264b82c22854
Shemrei Marabillo <shemrei@whyleavetown.com>
2025-09-09 14:37:08 +0800

    needs fixing with server and docker

---
+++ server/migrations/20250909063518-create-user.js
@@ -0,0 +1,51 @@
+"use strict";
+
+export async function up(queryInterface, Sequelize) {
+  await queryInterface.createTable("Users", {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    firstName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    userName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      defaultValue: "super-admin",
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  });
+}
+
+export async function down(queryInterface, Sequelize) {
+  await queryInterface.dropTable("Users");
+}
