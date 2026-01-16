a32c394881b8a164cfc8f1e5a0be8ec4fed9ffac
Shemrei Marabillo <shemrei@whyleavetown.com>
2026-01-16 09:13:46 +0800

    added stores to microsites payload

--- server/seeders/storeSeeder.js
+++ server/seeders/storeSeeder.js
@@ -1,29 +1,37 @@
 import db from "../models/index.js";
 
-const store = db.Store;
+const Store = db.Store;
 
 export const storeSeeder = async () => {
   try {
-    const existingStore = await store.findOne({
+    const existing = await Store.findOne({
       where: {
-        micrositeId: 4,
+        micrositeId: 1,
         name: "Annas Candles - Hand Crafted Soy Candles & Reed Diffusers",
       },
     });
-
-    if (existingStore) {
+    if (existing) {
       console.log("⚠️ Store already exists. Skipping seeding.");
       return;
     }
 
-    await store.create({
-      name: "Annas Candles - Hand Crafted Soy Candles & Reed Diffusers",
-      latitude: -28.8016841,
-      longitude: 151.8417212,
-      micrositeId: 1,
-    });
+    const stores = await Store.bulkCreate([
+      {
+        name: "Annas Candles - Hand Crafted Soy Candles & Reed Diffusers",
+        latitude: -28.8016841,
+        longitude: 151.8417212,
+        micrositeId: 1,
+      },
+      {
+        name: "Andersens Stanthorpe",
+        latitude: -28.6580644,
+        longitude: 151.9344512,
+        micrositeId: 1,
+      },
+    ]);
 
     console.log("✅ Store seeded successfully");
+    return stores;
   } catch (error) {
     console.error("❌ Error seeding store:", error);
   }
