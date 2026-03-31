f0688a55ff53f35948dea80f2eebe4933d794c5b
sbmwhylt <shemrei@whyleavetown.com>
2026-03-31 13:25:44 +0800

    removed gray card on dashboard

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -11,11 +11,10 @@ export default function Dashboard() {
           <NoticeBoard />
         </div>
         <div className="border p-4 aspect-video rounded-xl"></div>
-        <div className="aspect-video rounded-xl bg-gray-400" />
       </div>
 
       {/* Extra placeholder content */}
-      <div className="bg-muted/50 min-h-[50vh] flex-1 rounded-xl md:min-h-min " />
+      <div className=" min-h-[50vh] flex-1 rounded-xl md:min-h-min " />
     </MainLayout>
   );
 }
